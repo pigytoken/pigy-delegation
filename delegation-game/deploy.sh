fa260cb75506d3b77dcbbac03638648213a6b099
Brian W Bush <code@functionally.io>
2021-08-20 16:02:40 -0600

    relocated folder for PIGY delegation game

---
+++ delegation-game/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/b889a3f7a07515108cc9614639cd307cf2acbec5.tar.gz
+#!nix-shell -i bash -p ipfs postgresql
+
+set -ex
+
+. $HOME/secrets/ipfs.sh
+. $HOME/secrets/postgresql.sh
+
+psql -f compute-eligibility.sql
+
+gawk -f make-index.awk Eligibility.csv > pages/index.html
+
+gawk -f build-pages.awk Eligibility.csv
+
+DIR_CID=$(ipfs add --quiet --pin=false --recursive=true pages | tail -n 1)
+
+ipfs pin remote add --service=pinata --name=pigy $DIR_CID
+  
+ipfs name publish --key=pigy /ipfs/$DIR_CID
