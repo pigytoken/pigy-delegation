203924aebfd8f2132ec7288ba5360a80e40f7fcb
Brian W Bush <code@functionally.io>
2021-09-21 19:57:07 -0600

    hardened fetching script against API failures

--- oracle/fetch-data.sh
+++ oracle/fetch-data.sh
@@ -2,7 +2,6 @@
 #!nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/b889a3f7a07515108cc9614639cd307cf2acbec5.tar.gz
 #!nix-shell -i bash -p curl jq
 
-
 set -e
 
 TIMESTAMP=$(date --utc --rfc-3339=seconds | sed -e 's/ /T/')
@@ -13,32 +12,45 @@ DIR=data
 
 JSON=$DIR/$TIMESTAMP.json
 
-curl -s 'https://api.coingecko.com/api/v3/simple/price?ids=cardano,bitcoin,ethereum&vs_currencies=btc,eth,usd,eur,idr,gbp,jpy' \
-| tee tmp/coingeck.raw \
-| jq -f coingecko.jq \
-> tmp/coingecko.json
+{
+  curl -s 'https://markets.newyorkfed.org/api/rates/secured/sofr/last/1.json' \
+  |  tee tmp/nyfed.raw \
+  |  jq -f nyfed.jq \
+  || echo '{}'
+} 2> /dev/null > tmp/nyfed.json
 
-curl -s 'https://api.metals.live/v1/spot'  \
-| tee tmp/metalslive-spot.raw \
-| jq -f metalslive-spot.jq \
-> tmp/metalslive-spot.json
+{
+  curl -s 'https://api.coingecko.com/api/v3/simple/price?ids=cardano,bitcoin,ethereum&vs_currencies=btc,eth,usd,eur,idr,gbp,jpy' \
+  |  tee tmp/coingeck.raw \
+  |  jq -f coingecko.jq \
+  || echo '{}'
+} 2> /dev/null > tmp/coingecko.json
 
-curl -s 'https://api.metals.live/v1/spot/commodities'  \
-| tee tmp/metalslive-commodity.raw \
-| jq -f metalslive-commodity.jq \
-> tmp/metalslive-commodity.json
+{
+  curl -s 'https://api.metals.live/v1/spot' \
+  |  tee tmp/metalslive-spot.raw \
+  |  jq -f metalslive-spot.jq \
+  || echo '{}'
+} 2> /dev/null > tmp/metalslive-spot.json
 
-jq -s 'add | {metalslive: { source: "https://api.metals.live" , symbols: .}}' \
-  tmp/metalslive-spot.json \
-  tmp/metalslive-commodity.json \
-> tmp/metalslive.json
+{
+  curl -s 'https://api.metals.live/v1/spot/commodities' \
+  |  tee tmp/metalslive-commodity.raw \
+  |  jq -f metalslive-commodity.jq \
+  || echo '{}'
+} 2> /dev/null > tmp/metalslive-commodity.json
 
-curl -s 'https://markets.newyorkfed.org/api/rates/secured/sofr/last/1.json'  \
-| tee tmp/nyfed.raw \
-| jq -f nyfed.jq \
-> tmp/nyfed.json
+if [[ `stat -c %s tmp/metalslive-spot.json` -gt 3 ]] || [[ `stat -c %s tmp/metalslive-commodity.json` -gt 3 ]]
+then
+  jq -s 'add | {metalslive: {source: "https://api.metals.live", symbols: .}}' \
+    tmp/metalslive-spot.json \
+    tmp/metalslive-commodity.json \
+  > tmp/metalslive.json
+else
+  echo '{}' > tmp/metalslive.json
+fi
 
-jq -s 'add | {disclaimer: "ipfs://QmccBPKZqh9BJTJpC8oM6rc4gBrpcVXqcixX9KCsE6yDKd", oracle: "https://oracle.pigytoken.com", timestamp : "'$TIMESTAMP'", data: .}' \
+jq -s 'add | {disclaimer: "ipfs://QmccBPKZqh9BJTJpC8oM6rc4gBrpcVXqcixX9KCsE6yDKd", oracle: "https://oracle.pigytoken.com", timestamp: "'$TIMESTAMP'", data: .}' \
   tmp/nyfed.json  \
   tmp/coingecko.json \
   tmp/metalslive.json \
